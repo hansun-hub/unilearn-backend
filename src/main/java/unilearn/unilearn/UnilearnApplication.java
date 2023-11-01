e15bbdf9a85e4960fceaee9be3cb27c392314586
ppparkta <pppppark2@gmail.com>
2023-11-01 18:59:09 +0900

    Build: init project

---
+++ src/main/java/unilearn/unilearn/UnilearnApplication.java
@@ -0,0 +1,13 @@
+package unilearn.unilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnilearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnilearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unilearn/unilearn/UnilearnApplicationTests.java
@@ -0,0 +1,13 @@
+package unilearn.unilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnilearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
