ac964f9548e3b995d04157451754e718206f354f
ppparkta <pppppark2@gmail.com>
2023-11-02 01:47:27 +0900

    docs: principal 테스트 파일 주석처리

--- src/main/java/unilearn/unilearn/testService/Post.java
+++ src/main/java/unilearn/unilearn/testService/Post.java
@@ -1,23 +1,23 @@
-package unilearn.unilearn.testService;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import unilearn.unilearn.user.domain.User;
-
-import javax.persistence.*;
-
-@Entity
-@Getter @Builder
-@AllArgsConstructor @NoArgsConstructor
-public class Post {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String author;
-    private String body;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
-}
+//package unilearn.unilearn.testService;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import unilearn.unilearn.user.domain.User;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Getter @Builder
+//@AllArgsConstructor @NoArgsConstructor
+//public class Post {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String author;
+//    private String body;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+//}
--- src/main/java/unilearn/unilearn/testService/PostContoller.java
+++ src/main/java/unilearn/unilearn/testService/PostContoller.java
@@ -1,49 +1,53 @@
-package unilearn.unilearn.testService;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import unilearn.unilearn.user.domain.User;
-import unilearn.unilearn.user.repository.UserRepository;
-
-import javax.validation.Valid;
-import java.security.Principal;
-
-@RestController
-@Slf4j
-@RequiredArgsConstructor
-public class PostContoller {
-    private final PostRepository postRepository;
-    private final UserRepository userRepository;
-
-    @PostMapping("/posts")
-    public ResponseEntity<PostDto> create(@Valid @RequestBody PostDto postDto, Principal principal) {
-        if (principal.getName() != null){
-            System.out.println(principal.getName() + principal);
-        }
-        else{
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
-        User user = userRepository.findByNickname(principal.getName());
-        log.info("principal user = " + user.toString());
-        Post post = Post.builder()
-                .user(user)
-                .author(postDto.getAuthor())
-                .body(postDto.getBody())
-                .build();
-        Post savePost = postRepository.save(post);
-
-        PostDto returnDto = new PostDto();
-        returnDto.setUser(savePost.getUser().getId());
-        returnDto.setAuthor(savePost.getAuthor());
-        returnDto.setBody(savePost.getBody());
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(returnDto);
-
-    }
-}
+//package unilearn.unilearn.testService;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//import unilearn.unilearn.user.domain.User;
+//import unilearn.unilearn.user.repository.UserRepository;
+//
+//import javax.validation.Valid;
+//import java.security.Principal;
+//
+//@RestController
+//@Slf4j
+//@RequiredArgsConstructor
+//public class PostContoller {
+//    private final PostRepository postRepository;
+//    private final UserRepository userRepository;
+//
+//    @PostMapping("/posts")
+//    public ResponseEntity<PostDto> create(@Valid @RequestBody PostDto postDto, BindingResult bindingResult, Principal principal) {
+//        if (principal.getName() != null){
+//            System.out.println(principal.getName() + principal);
+//        }
+//        else if(bindingResult.hasErrors()) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+//        }
+//        else {
+//            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+//        }
+//        User user = userRepository.findByNickname(principal.getName());
+//        log.info("principal user = " + user.toString());
+//        Post post = Post.builder()
+//                .user(user)
+//                .author(postDto.getAuthor())
+//                .body(postDto.getBody())
+//                .build();
+//        Post savePost = postRepository.save(post);
+//
+//        PostDto returnDto = new PostDto();
+//        returnDto.setUser(savePost.getUser().getId());
+//        returnDto.setAuthor(savePost.getAuthor());
+//        returnDto.setBody(savePost.getBody());
+//
+//        return ResponseEntity.status(HttpStatus.CREATED).body(returnDto);
+//
+//    }
+//}
