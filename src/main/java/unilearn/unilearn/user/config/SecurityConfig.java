3068d28a985571211f7d569ce9f27f749b1bcf78
ppparkta <pppppark2@gmail.com>
2023-11-26 23:31:23 +0900

    feat: cors config

--- src/main/java/unilearn/unilearn/user/config/SecurityConfig.java
+++ src/main/java/unilearn/unilearn/user/config/SecurityConfig.java
@@ -32,12 +32,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().disable()
                 .httpBasic().disable() // rest api -> 기본 설정 disable
                 .csrf().disable() // csrf -> 비활성화, security 설정 시 기본값으로 활성화 되어있음
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/login", "/signup").permitAll()
+                .antMatchers("/login", "/signup", "/h2-console").permitAll()
                 .anyRequest().hasRole("USER") // 나머지 요청에 대해 USER ROLE 을 가져야만 접근 가능
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
